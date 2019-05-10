cb9ecc8db925122e77bca51f06ce7d1f220106bf
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-05-10 17:32:39 +0530

    feat: Add more query parameters to some endpoints

--- spacex-api-wrapper/src/main/java/com/haroldadmin/spacex_api_wrapper/launches/LaunchesService.kt
+++ spacex-api-wrapper/src/main/java/com/haroldadmin/spacex_api_wrapper/launches/LaunchesService.kt
@@ -5,20 +5,57 @@ import com.haroldadmin.spacex_api_wrapper.common.ErrorResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface LaunchesService {
 
     @GET("/launches")
-    fun getAllLaunches(): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
+    fun getAllLaunches(
+        @Query("flight_id") flightId: String? = null,
+        @Query("start") start: String? = null,
+        @Query("end") end: String? = null,
+        @Query("land_success") landSucess: Boolean? = null,
+        @Query("site_id") siteId: String? = null,
+        @Query("customer") customer: String? = null,
+        @Query("nationality") nationality: String? = null,
+        @Query("launch_success") launchSuccess: Boolean? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("sort") sort: String = "original_launch",
+        @Query("order") order: String = "desc"
+    ): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
 
     @GET("/launches/{flightNumber}")
     fun getLaunch(@Path("flightNumber") flightNumber: Int): Deferred<NetworkResponse<Launch, ErrorResponse>>
 
     @GET("/launches/past")
-    fun getPastLaunches(): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
+    fun getPastLaunches(
+        @Query("flight_id") flightId: String? = null,
+        @Query("start") start: String? = null,
+        @Query("end") end: String? = null,
+        @Query("land_success") landSucess: Boolean? = null,
+        @Query("site_id") siteId: String? = null,
+        @Query("customer") customer: String? = null,
+        @Query("nationality") nationality: String? = null,
+        @Query("launch_success") launchSuccess: Boolean? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("sort") sort: String = "original_launch",
+        @Query("order") order: String = "desc"
+    ): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
 
     @GET("/launches/upcoming")
-    fun getUpcomingLaunches(): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
+    fun getUpcomingLaunches(
+        @Query("flight_id") flightId: String? = null,
+        @Query("start") start: String? = null,
+        @Query("end") end: String? = null,
+        @Query("land_success") landSucess: Boolean? = null,
+        @Query("site_id") siteId: String? = null,
+        @Query("customer") customer: String? = null,
+        @Query("nationality") nationality: String? = null,
+        @Query("launch_success") launchSuccess: Boolean? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("sort") sort: String = "original_launch",
+        @Query("order") order: String = "asc"
+    ): Deferred<NetworkResponse<List<Launch>, ErrorResponse>>
 
     @GET("/launches/latest")
     fun getLatestLaunch(): Deferred<NetworkResponse<Launch, ErrorResponse>>
--- spacex-api-wrapper/src/main/java/com/haroldadmin/spacex_api_wrapper/rocket/RocketsService.kt
+++ spacex-api-wrapper/src/main/java/com/haroldadmin/spacex_api_wrapper/rocket/RocketsService.kt
@@ -5,15 +5,15 @@ import com.haroldadmin.spacex_api_wrapper.common.ErrorResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RocketsService {
 
     @GET("/rockets")
-    fun getAllRockets(): Deferred<NetworkResponse<List<Rocket>, ErrorResponse>>
+    fun getAllRockets(
+        @Query("limit") limit: Int? = null
+    ): Deferred<NetworkResponse<List<Rocket>, ErrorResponse>>
 
     @GET("/rockets/{rocketId}")
     fun getRocket(@Path("rocketId") rocketId: String): Deferred<NetworkResponse<Rocket, ErrorResponse>>
-
-
-
 }
