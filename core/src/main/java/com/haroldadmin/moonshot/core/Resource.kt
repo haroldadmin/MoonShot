44a2cbe7d16e1e2090ab6f302ddd0569603611b8
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-05-24 14:57:56 +0530

    fix: Fix generic variance in Resource class

--- core/src/main/java/com/haroldadmin/moonshot/core/Resource.kt
+++ core/src/main/java/com/haroldadmin/moonshot/core/Resource.kt
@@ -3,20 +3,22 @@ package com.haroldadmin.moonshot.core
 /**
  * A sealed class to represent UI states associated with a resource.
  */
-sealed class Resource<T> {
+sealed class Resource<out T> {
 
     /**
      * A data class to represent the scenario where the resource is available without any errors
      */
-    data class Success <T>(val data: T): Resource<T>()
+    data class Success <out T>(val data: T): Resource<T>()
 
     /**
      * A data class to represent the scenario where a resource may or may not be available due to an error
      */
-    data class Error <T, E> (val data: T?, val error: E): Resource<T>()
+    data class Error <out T, out E> (val data: T?, val error: E?): Resource<T>()
 
     /**
      * A class to represent the loading state of an object
      */
-    class Loading<T>: Resource<T>()
+    object Loading: Resource<Nothing>()
+
+    object Unitialized: Resource<Nothing>()
 }
