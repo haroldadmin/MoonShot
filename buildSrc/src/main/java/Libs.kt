36ab7927e322415b7767c96bd5938df0eaa1b392
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-05-24 14:35:04 +0530

    chore: Add deps to buildSrc

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -14,8 +14,14 @@ object Libs {
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val ktxCore = "androidx.core:core-ktx:${Versions.ktxCore}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val materialComponents = "com.google.android.material:material:${Versions.materialComponents}"
+    const val navigation = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+    const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
 
     const val mvrx = "com.airbnb.android:mvrx:${Versions.mvrx}"
+    const val epoxy = "com.airbnb.android:epoxy:${Versions.epoxy}"
+    const val epoxyProcessor = "com.airbnb.android:epoxy-processor:${Versions.epoxy}"
 
     const val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
     const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
@@ -25,6 +31,7 @@ object Libs {
     const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
 
     const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
     const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
     const val networkResponseAdapter = "com.github.haroldadmin:CoroutinesNetworkResponseAdapter:${Versions.cnrAdapter}"
     const val moshi = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
@@ -33,9 +40,10 @@ object Libs {
     const val moshiAdapters = "com.squareup.moshi:moshi-adapters:${Versions.moshiAdapters}"
     const val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.mockWebServer}"
 
-    const val junit = "junit:junit:${Versions.junit}"
+    const val junit4 = "junit:junit:${Versions.junit4}"
     const val androidxJunitExt = "androidx.test.ext:junit:${Versions.androidxJunitExt}"
     const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
     const val kotlinTest = "io.kotlintest:kotlintest-runner-junit5:${Versions.kotlinTest}"
     const val androidxTestCore = "androidx.arch.core:core-testing:${Versions.androidxTestCore}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,8 +7,11 @@ object Versions {
     const val appCompat = "1.0.2"
     const val ktxCore = "1.0.2"
     const val constraintLayout = "1.1.3"
+    const val materialComponents = "1.1.0-alpha06"
+    const val navigation = "2.1.0-alpha04"
 
     const val mvrx = "1.0.1"
+    const val epoxy = "3.5.1"
 
     const val rxJava = "2.2.8"
     const val rxAndroid = "2.1.1"
@@ -16,6 +19,7 @@ object Versions {
     const val room = "2.1.0-alpha07"
 
     const val retrofit = "2.5.0"
+    const val okHttp = "3.14.2"
     const val loggingInterceptor = "3.14.1"
     const val cnrAdapter = "2.0"
     const val moshi = "1.8.0"
@@ -23,9 +27,10 @@ object Versions {
     const val moshiAdapters = "1.8.0"
     const val mockWebServer = "3.12.0"
 
-    const val junit = "4.12"
+    const val junit4 = "4.12"
     const val androidxJunitExt = "1.1.0"
     const val espressoCore = "3.1.1"
     const val kotlinTest = "3.3.2"
     const val androidxTestCore = "2.0.0-rc01"
+    const val mockk = "1.9.3"
 }
