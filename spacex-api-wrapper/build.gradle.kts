85ae261894b4979be14e5cea00b05e06bee3ba2a
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-05-24 14:32:15 +0530

    refactor: Convert CapsuleTest to use KotlinTest

--- spacex-api-wrapper/build.gradle.kts
+++ spacex-api-wrapper/build.gradle.kts
@@ -11,20 +11,20 @@ val test by tasks.getting(Test::class) {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(Libs.kotlinStdLib)
+    implementation(Libs.kotlinReflect)
     implementation(Libs.coroutines)
 
     implementation(Libs.koinCore)
 
     implementation(Libs.retrofit)
     implementation(Libs.loggingInterceptor)
-    api(Libs.networkResponseAdapter)
+    implementation(Libs.networkResponseAdapter)
     implementation(Libs.moshi)
     implementation(Libs.moshiConverter)
     implementation (Libs.moshiAdapters)
     kapt(Libs.moshiCodegen)
 
     testImplementation(Libs.mockWebServer)
-    testImplementation(Libs.junit)
     testImplementation(Libs.koinTest)
     testImplementation(Libs.kotlinTest)
 }
--- spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/capsule/CapsuleTest.kt
+++ spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/capsule/CapsuleTest.kt
@@ -1,67 +1,49 @@
 package com.haroldadmin.spacex_api_wrapper.capsule
 
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.haroldadmin.spacex_api_wrapper.getResource
-import com.haroldadmin.spacex_api_wrapper.testModule
-import kotlinx.coroutines.runBlocking
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.MockWebServer
-import org.junit.After
-import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.core.parameter.parametersOf
-import org.koin.test.KoinTest
-import org.koin.test.get
-import retrofit2.Retrofit
+import com.haroldadmin.spacex_api_wrapper.BaseApiTest
+import com.haroldadmin.spacex_api_wrapper.enqueue
+import com.haroldadmin.spacex_api_wrapper.fromFile
+import com.haroldadmin.spacex_api_wrapper.launches.Launch
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.types.shouldBeTypeOf
+import io.kotlintest.shouldBe
 
-internal class CapsuleTest : KoinTest {
+internal class CapsuleTest: BaseApiTest() {
 
-    lateinit var server: MockWebServer
-    lateinit var service: CapsuleService
+    private val service by lazy { retrofit.create(CapsuleService::class.java) }
 
-    @Before
-    fun setup() {
-        startKoin { modules(testModule) }
-        server = MockWebServer()
-        server.start()
-        service = get<Retrofit> { parametersOf(server) }.create(CapsuleService::class.java)
-    }
+    init {
 
-    @After
-    fun teardown() {
-        stopKoin()
-        server.close()
-    }
+        describe("Capsules Service") {
+            context("All capsules request") {
+                val responseBody = server.enqueue { fromFile("/sampledata/capsules/all_capsules_response.json") }
+                val response = service.getAllCapsules().await()
 
-    @Test
-    fun `Get all capsules should be return a list of capsules`() = runBlocking {
-        val responseBody: String = getResource("/sampledata/capsules/all_capsules_response.json").readText()
-        server.enqueue(
-            MockResponse().apply {
-                setBody(responseBody)
-                setResponseCode(200)
+                it ("Should return successfully") {
+                    (response is NetworkResponse.Success) shouldBe true
+                }
+
+                it ("Should parse sample data correctly") {
+                    (response as NetworkResponse.Success).body shouldHaveSize 18
+                }
             }
-        )
-        val response = service.getAllCapsules().await()
-        assertTrue(response is NetworkResponse.Success)
-        assertTrue((response as NetworkResponse.Success).body.isNotEmpty())
+
+            context("One capsule request") {
+                val responseBody = server.enqueue { fromFile("/sampledata/capsules/one_capsule_response.json") }
+                val serial = "C112"
+                val response = service.getCapsule(serial).await()
+
+                it("Should return successfully") {
+                    response.shouldBeTypeOf<NetworkResponse.Success<Launch>>()
+                }
+
+                it("Should parse sample data correctly") {
+                    (response as NetworkResponse.Success).body.serial shouldBe serial
+                }
+            }
+        }
+
     }
 
-    @Test
-    fun `Get one capsule should return the capsule with the requested ID`() = runBlocking {
-        val responseBody = getResource("/sampledata/capsules/one_capsule_response.json").readText()
-        server.enqueue(
-            MockResponse().apply {
-                setBody(responseBody)
-                setResponseCode(200)
-            }
-        )
-        val serial = "C112"
-        val response = service.getCapsule(serial).await()
-        assertTrue(response is NetworkResponse.Success)
-        assertTrue((response as NetworkResponse.Success<Capsule>).body.serial == serial)
-    }
 }
