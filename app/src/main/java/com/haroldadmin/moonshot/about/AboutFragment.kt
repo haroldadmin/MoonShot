85cd63dc33783bb3e4a8b3dbe240f2f1c59841f4
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-06-15 13:23:21 +0530

    fix: Fix lint errors

--- app/src/main/java/com/haroldadmin/moonshot/about/AboutFragment.kt
+++ app/src/main/java/com/haroldadmin/moonshot/about/AboutFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.GridLayoutManager
 import com.haroldadmin.moonshot.R
 import com.haroldadmin.moonshot.base.MoonShotFragment
 import com.haroldadmin.moonshot.base.simpleController
