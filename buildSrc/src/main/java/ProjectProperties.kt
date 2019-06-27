f9bb99dc2406e38bb64f4c60b09bef4b875dc06c
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-06-27 09:53:55 +0530

    chore: Upgrade AGP

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -8,7 +8,7 @@ object ProjectProperties {
     const val versionName = "0.7.0"
 
     const val kotlinVersion = "1.3.40"
-    const val agpVersion = "3.5.0-beta04"
+    const val agpVersion = "3.5.0-beta05"
 
     const val gmsVersion = "4.2.0"
     const val fabricVersion = "1.29.0"
