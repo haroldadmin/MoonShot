52f5e7c2bf38956d6f2feb9ed5c5dba0a61077e8
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-07-01 14:10:19 +0530

    chore: Bump version

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -4,8 +4,8 @@ object ProjectProperties {
     const val targetSdk = 29
 
     const val applicationId = "com.haroldadmin.moonshot"
-    const val versionCode = 18
-    const val versionName = "0.7.0"
+    const val versionCode = 19
+    const val versionName = "0.8.0"
 
     const val kotlinVersion = "1.3.40"
     const val agpVersion = "3.5.0-beta05"
