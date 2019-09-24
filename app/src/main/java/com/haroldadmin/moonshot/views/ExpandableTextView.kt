427292bd8a7351850d214af4b388c6958f3bc426
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-09-24 14:06:29 +0530

    fix: Switch to char count to decide if expansion should be allowed

--- app/src/main/java/com/haroldadmin/moonshot/views/ExpandableTextView.kt
+++ app/src/main/java/com/haroldadmin/moonshot/views/ExpandableTextView.kt
@@ -3,6 +3,7 @@ package com.haroldadmin.moonshot.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -20,6 +21,17 @@ import com.haroldadmin.moonshot.base.gone
 import com.haroldadmin.moonshot.base.show
 import com.haroldadmin.moonshot.utils.asyncText
 
+/**
+ * A compound view which supports expanding/collapsing a text view containing long text.
+ *
+ * When the content text is supplied to this class, it checks if it's length is greater than
+ * a predefined character limit. If it is, then it sets the maxLines attribute on the textView to a predefined
+ * small integer constant.
+ *
+ * We don't use TextView's lineCount attribute to decide if the text is too long, because it sometimes returns 0
+ * just after the text is set. Also, by relying on a predefined max character count value we can also use
+ * Precomputed Text.
+ */
 @ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
 class ExpandableTextView @JvmOverloads constructor(
     context: Context,
@@ -34,20 +46,20 @@ class ExpandableTextView @JvmOverloads constructor(
     private val headerText: AppCompatTextView = findViewById(R.id.headingText)
     private val contentText: AppCompatTextView = findViewById(R.id.contentText)
     private val toggleIcon: ImageView = findViewById(R.id.toggleIcon)
+
     private val collapsedIcon = context.getDrawable(R.drawable.ic_round_expand_more_24px)
     private val expandedIcon = context.getDrawable(R.drawable.ic_round_expand_less_24px)
 
+    private val maxChars = context.resources.getInteger(R.integer.maxCollapsedChars)
     private val shrunkLines = context.resources.getInteger(R.integer.maxLinesCollapsed)
     private val transitionDuration = context.resources.getInteger(R.integer.expandableTextToggleDuration)
     private var header: CharSequence = ""
     private var content: CharSequence = ""
 
-    private var isExpanded: Boolean = false
     private val shouldAllowToggle: Boolean
-        get() {
-            return true
-//            return content.isNotEmpty() && contentText.lineCount > shrunkLines
-        }
+        get() { return content.length > maxChars }
+
+    private var isExpanded: Boolean = false
 
     private val transition: Transition = ChangeBounds().apply {
         duration = transitionDuration.toLong()
@@ -62,12 +74,10 @@ class ExpandableTextView @JvmOverloads constructor(
         if (isExpanded) {
             contentText.maxLines = shrunkLines
             toggleIcon.setImageDrawable(collapsedIcon)
-
             isExpanded = false
         } else {
             contentText.maxLines = Integer.MAX_VALUE
             toggleIcon.setImageDrawable(expandedIcon)
-
             isExpanded = true
         }
     }
@@ -87,16 +97,20 @@ class ExpandableTextView @JvmOverloads constructor(
 
         headerText.asyncText(header)
         contentText.asyncText(content)
-        contentText.maxLines = shrunkLines
 
-        if (!shouldAllowToggle) {
-            toggleIcon.gone()
-        } else {
+        if (content.length > maxChars) {
+            contentText.maxLines = shrunkLines
             toggleIcon.apply {
                 show()
                 setImageDrawable(collapsedIcon)
             }
+            isExpanded = false
             rootView.setOnClickListener(toggleClickListener)
+        } else {
+            isExpanded = true
+            toggleIcon.apply {
+                gone()
+            }
         }
     }
 
