1d484edb5e120a9a6ed5e9b14bd0382384cc4d70
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-07-11 00:04:05 +0530

    feat: Add v4 roadster models and test

---
+++ spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/v4/RoadsterModelTest.kt
@@ -0,0 +1,36 @@
+package com.haroldadmin.spacex_api_wrapper.v4
+
+import com.haroldadmin.spacex_api_wrapper.getResource
+import com.squareup.moshi.Moshi
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.types.shouldNotBeNull
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.AnnotationSpec
+import java.time.Month
+
+internal class RoadsterModelTest : AnnotationSpec() {
+
+    private lateinit var moshi: Moshi
+
+    @Before
+    fun setup() {
+        moshi = Moshi.Builder().add(ZonedDateTimeAdapter()).build()
+    }
+
+    @Test
+    fun testRoadsterModel() {
+        val roadsterJson = getResource("/sampledata/v4/roadster_info.json").readText()
+        val roadsterAdapter = moshi.adapter(RoadsterInfo::class.java)
+        val roadster = roadsterAdapter.fromJson(roadsterJson)
+
+        with(roadster) {
+            this.shouldNotBeNull()
+            flickrImages shouldHaveSize 4
+            launchDateUTC?.year shouldBe 2018
+            launchDateUTC?.month shouldBe Month.FEBRUARY
+            launchDateUTC?.dayOfMonth shouldBe 6
+            launchMassKg shouldBe 1350.0
+        }
+    }
+
+}
