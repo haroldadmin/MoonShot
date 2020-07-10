3ac55765341f285412d7c43f567d64c327f16e44
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-07-10 16:31:15 +0530

    feat: Add updated CompanyInfo model and tests

--- spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/v4/LaunchesModelTest.kt
+++ spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/v4/LaunchesModelTest.kt
@@ -33,13 +33,13 @@ internal class LaunchesModelTest {
             with(links) {
                 this.shouldNotBeNull()
                 patch.shouldNotBeNull()
-                patch?.small.shouldBe("https://images2.imgbox.com/53/22/dh0XSLXO_o.png")
-                patch?.large.shouldBe("https://images2.imgbox.com/15/2b/NAcsTEB6_o.png")
+                patch?.small shouldBe "https://images2.imgbox.com/53/22/dh0XSLXO_o.png"
+                patch?.large shouldBe "https://images2.imgbox.com/15/2b/NAcsTEB6_o.png"
 
                 reddit.shouldNotBeNull()
-                reddit?.campaign.shouldBe("https://www.reddit.com/r/spacex/comments/ezn6n0/crs20_launch_campaign_thread")
-                reddit?.launch.shouldBe("https://www.reddit.com/r/spacex/comments/fe8pcj/rspacex_crs20_official_launch_discussion_updates/")
-                reddit?.media.shouldBe("https://www.reddit.com/r/spacex/comments/fes64p/rspacex_crs20_media_thread_videos_images_gifs/")
+                reddit?.campaign shouldBe "https://www.reddit.com/r/spacex/comments/ezn6n0/crs20_launch_campaign_thread"
+                reddit?.launch shouldBe "https://www.reddit.com/r/spacex/comments/fe8pcj/rspacex_crs20_official_launch_discussion_updates/"
+                reddit?.media shouldBe "https://www.reddit.com/r/spacex/comments/fes64p/rspacex_crs20_media_thread_videos_images_gifs/"
             }
 
             launchDateUTC.toInstant()
