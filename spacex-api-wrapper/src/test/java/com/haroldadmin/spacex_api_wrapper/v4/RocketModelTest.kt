01f8c86b15cbba2485b5a114fb4120c76ee44709
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2020-07-11 00:46:38 +0530

    feat: Add v4 Rocket model and test

---
+++ spacex-api-wrapper/src/test/java/com/haroldadmin/spacex_api_wrapper/v4/RocketModelTest.kt
@@ -0,0 +1,54 @@
+package com.haroldadmin.spacex_api_wrapper.v4
+
+import com.haroldadmin.spacex_api_wrapper.getResource
+import com.squareup.moshi.Moshi
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.matchers.types.shouldNotBeNull
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.AnnotationSpec
+
+internal class RocketModelTest: AnnotationSpec() {
+
+    private lateinit var moshi: Moshi
+
+    @Before
+    fun setup() {
+        moshi = Moshi.Builder().build()
+    }
+
+    @Test
+    fun testRocketModel() {
+        val rocketJson = getResource("/sampledata/v4/one_rocket.json").readText()
+        val rocketAdapter = moshi.adapter(Rocket::class.java)
+        val rocket = rocketAdapter.fromJson(rocketJson)
+
+        with(rocket) {
+            this.shouldNotBeNull()
+
+            height?.metres shouldBe 70.0
+            diameter?.metres shouldBe 12.2
+            mass?.kg shouldBe 1420788.0
+
+            firstStage?.thrustSeaLevel?.kN shouldBe 22819.0
+            firstStage?.thrustVacuum?.kN shouldBe 24681.0
+            firstStage?.reusable shouldBe true
+
+            secondStage?.thrust?.kN shouldBe 934.0
+            secondStage?.payloads?.compositeFairing?.height?.metres shouldBe 13.1
+            secondStage?.payloads?.compositeFairing?.diameter?.metres shouldBe 5.2
+            secondStage?.payloads?.optionOne shouldBe "dragon"
+
+            engines?.isp?.seaLevel shouldBe 288.0
+            engines?.thrustSeaLevel?.kN shouldBe 845.0
+            engines?.thrustVacuum?.kN shouldBe 914.0
+            engines?.layout shouldBe "octaweb"
+
+            landingLegs?.number shouldBe 12
+            payloadWeights shouldHaveSize 4
+            flickrImages shouldHaveSize 4
+
+            id shouldBe "5e9d0d95eda69974db09d1ed"
+        }
+    }
+
+}
