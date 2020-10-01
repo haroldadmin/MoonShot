dbda389b5ff913bad6a563df703a897d30dab443
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-10-01 17:37:13 +0530

    Migrate Epoxy To v4.0.0

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -22,7 +22,7 @@ object Versions {
     const val openGraphKt = "master-SNAPSHOT"
     const val whatTheStack = "0.0.4"
 
-    const val epoxy = "3.9.0"
+    const val epoxy = "4.0.0"
 
     const val coil = "0.9.2"
 
